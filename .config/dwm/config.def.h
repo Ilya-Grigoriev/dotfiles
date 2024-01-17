d85c4d9113326525fad7793bbf7c0c10579b016f
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2024-01-18 02:54:12 +0300

    feat(dwm): added more colors

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 6;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -9,14 +9,16 @@ static const char *fonts[]          = { "monospace:size=13" };
 static const char dmenufont[]       = "monospace:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_purple[]      = "#864AF9";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_purple1[]     = "#864AF9";
+static const char col_purple2[]     = "#7E30E1";
+static const char col_blue1[]       = "#3B3486";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_purple  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_purple1  },
 };
 
 /* tagging */
