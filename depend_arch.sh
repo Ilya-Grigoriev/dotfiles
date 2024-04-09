600ef2b7284a0bb90efd5df83c00858bc74b230f
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-04-09 23:13:14 +0300

    refactor(depend): made some files with scripts for certain OS

--- depend_arch.sh
+++ depend_arch.sh
@@ -5,10 +5,11 @@ if ! [ -f "/usr/bin/yay" ]; then
 	echo "yay installed"
 fi
 
-yay -S obsidian-bin thorium-browser-bin fd ttf-jetbrains-mono-git
+yay -S obsidian-bin thorium-browser-bin fd ttf-jetbrains-mono-git nsxiv sent
 sudo pacman -S htop bc vim vi lolcat grep fzf cmus nnn gimp dmenu zathura npm rust github-cli less neofetch tree lua obs-studio mpv man wpa_supplicant-openrc dhcpcd-openrc
 
-echo "Setting up network..."
+
+echo "Setting up network..." | lolcat
 sudo rc-update add dhcpcd default
 sudo rc-service dhcpcd start
 
@@ -18,8 +19,8 @@ sudo rc-service wpa_supplicant start
 sudo rc-update add wpa_supplicant default
 sudo rc-service wpa_supplicant start
 
-echo "Setting up audio..."
+
+echo "Setting up audio..." | lolcat
 sudo rc-update add alsasound default
 sudo rc-service alsasound start
-
 sudo gpasswd $(echo $USER) audio
--- rocks.sh
+++ rocks.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-lh cm-super
 
 mkdir -p ~/.local/share
 mkdir -p ~/.config
@@ -16,29 +15,6 @@ echo "Setting up aliases..."
 mkdir -p ~/.config/aliases
 cp .config/aliases/* ~/.config/aliases
 echo "Setting up ended"
-
-
-# ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
-############################################
-# ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-
-if [ -z "$(dpkg --list | grep 'deb-pacman')" ]
-then
-	echo "Installing pacman..."
-	# Source: https://www.cyberithub.com/how-to-install-pacman-package-manager-on-ubuntu-20-04-lts/
-	wget https://gitlab.com/trivoxel-utils/deb-pacman/uploads/460d83f8711c1ab5e16065e57e7eeabc/deb-pacman-2.0-0.deb
-	sudo dpkg -i deb-pacman-2.0-0.deb
-	rm -rf deb-pacman-*
-	echo "Pacman installed!"
-fi
-
-
-# ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
-############################################
-# ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
-
-
-sudo pacman -S zathura cmake zsh curl neofetch lolcat ripgrep xclip fzf tldr libxft-dev libx11-dev cmus gh pass feh kdeconnect tmux libx11-dev mesa-common-dev libglu1-mesa-dev libxrandr-dev libxi-dev libxinerama-dev htop mpv obs-studio suckless-tools synaptic network-manager fontforge pipx python3-pip flameshot entr pandoc libimlib2-dev libexif-dev abiword fd-find meson ninja-build fonts-recommended libgtk-3-dev pulseaudio alsa-utils libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev bc syncthing libcairo2-dev npm upower compton lua-check ruby-dev nnn adb xorg bat libreoffice-base mupdf bison libncurses-dev ntfs-3g kolourpaint tk tk-dev screenkey gimp
 
 
 # ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
