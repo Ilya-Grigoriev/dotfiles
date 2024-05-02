5906dbbe7b94e8eb2eeb3fb6cbfc4418dc7dbecc
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-05-02 21:26:00 +0300

    feat(depends_artix): added new packages

--- depends_artix.sh
+++ depends_artix.sh
@@ -7,7 +7,7 @@ if ! [ -f "/usr/bin/yay" ]; then
 fi
 
 yay -S obsidian-bin thorium-browser-bin fd-git ttf-jetbrains-mono-git nsxiv sent
-sudo pacman -S htop bc vim vi lolcat grep fzf cmus nnn gimp dmenu zathura zathura-pdf-poppler npm rust github-cli less neofetch tree lua obs-studio mpv man wpa_supplicant-openrc dhcpcd-openrc networkmanager-openrc man wget ripgrep usbutils alsa-utils alsa-firmware pulseaudio-alsa pulseaudio-bluetooth syncthing-openrc entr pandoc flameshot yt-dlp sof-firmware unzip xorg xorg-xinit feh xf86-video-intel xsel openssh xclip tldr fzf imagemagick
+sudo pacman -S htop bc vim vi lolcat grep fzf cmus nnn gimp dmenu zathura zathura-pdf-poppler npm rust github-cli less neofetch tree lua obs-studio mpv man wpa_supplicant-openrc dhcpcd-openrc networkmanager-openrc man wget ripgrep usbutils alsa-utils alsa-firmware pulseaudio-alsa pulseaudio-bluetooth syncthing-openrc entr pandoc flameshot yt-dlp sof-firmware unzip xorg xorg-xinit feh xf86-video-intel xsel openssh xclip tldr fzf imagemagick bash-completion bluez bluez-utils bluez-openrc
 
 
 echo "Setting up network..." | lolcat
@@ -20,6 +20,9 @@ sudo rc-service wpa_supplicant start
 sudo rc-update add NetworkManager default
 sudo rc-service NetworkManager start
 
+echo "Setting up bluetooth..." | lolcat
+sudo rc-update add bluetoothd default
+sudo rc-service bluetoothd start
 
 echo "Setting up audio..." | lolcat
 sudo rc-update add alsasound default
