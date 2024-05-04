d915a1d4b10409f3b0778636c491e339a6f33da8
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-05-04 20:43:07 +0300

    feat(depends_artix): added new packages

--- depends_artix.sh
+++ depends_artix.sh
@@ -7,7 +7,7 @@ if ! [ -f "/usr/bin/yay" ]; then
 fi
 
 yay -S obsidian-bin thorium-browser-bin fd-git ttf-jetbrains-mono-git nsxiv sent
-sudo pacman -S htop bc vim vi lolcat grep fzf cmus nnn gimp dmenu zathura zathura-pdf-poppler npm rust github-cli less neofetch tree lua obs-studio mpv man wpa_supplicant-openrc dhcpcd-openrc networkmanager-openrc man wget ripgrep usbutils alsa-utils alsa-firmware pulseaudio-alsa pulseaudio-bluetooth syncthing-openrc entr pandoc flameshot yt-dlp sof-firmware unzip xorg xorg-xinit feh xf86-video-intel xsel openssh xclip tldr fzf imagemagick bash-completion bluez bluez-utils bluez-openrc
+sudo pacman -S htop bc vim vi lolcat grep fzf cmus nnn gimp dmenu zathura zathura-pdf-poppler npm rust github-cli less neofetch tree lua obs-studio mpv man wpa_supplicant-openrc dhcpcd-openrc networkmanager-openrc man wget ripgrep usbutils alsa-utils alsa-firmware pulseaudio-alsa pulseaudio-bluetooth syncthing-openrc entr pandoc flameshot yt-dlp sof-firmware unzip xorg xorg-xinit feh xf86-video-intel xsel openssh xclip tldr fzf imagemagick bash-completion bluez bluez-utils bluez-openrc libreoffice-fresh
 
 
 echo "Setting up network..." | lolcat
